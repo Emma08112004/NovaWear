da2ba23c16ecdf21c3dc7e9eeb61c4c8edb6bd27
Satinemoati <satinemoati@gmail.com>
2025-03-17 10:19:31 +0100

    Mise à jour des templates

---
+++ src/Controller/MainController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('main/home.html.twig');
+    }
+
+    #[Route('/inscription', name: 'inscription')]
+    public function inscription(): Response
+    {
+        return $this->render('main/inscription.html.twig');
+    }
+
+    #[Route('/connexion', name: 'connexion')]
+    public function connexion(): Response
+    {
+        return $this->render('main/connexion.html.twig');
+    }
+
+    #[Route('/panier', name: 'panier')]
+    public function panier(): Response
+    {
+        return $this->render('main/panier.html.twig');
+    }
+
+    #[Route('/paiement', name: 'paiement')]
+    public function paiement(): Response
+    {
+        return $this->render('main/paiement.html.twig');
+    }
+
+    #[Route('/recapitulatif', name: 'recapitulatif')]
+    public function recapitulatif(): Response
+    {
+        return $this->render('main/recapitulatif.html.twig');
+    }
+}
